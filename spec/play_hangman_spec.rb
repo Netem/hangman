df271d1027aaa780830e2e20add11881a9cc5aeb
Hanna Hagenmalm <sorida.lava@gmail.com>
2014-02-25 13:34:19 +0100

    Built an integration test for play_hangman.rb.
    It tests the majority of the file, but not the loop.
    
    Signed-off-by: Hanna Hagenmalm <sorida.lava@gmail.com>

--- spec/play_hangman_spec.rb
+++ spec/play_hangman_spec.rb
@@ -4,25 +4,35 @@ require_relative "spec_helper.rb"
 
 describe PlayHangman do
 
-  context "winning scenario" do
-    it "returns true" do
-      @test = PlayHangman.new
-      @test.play.secret_word = %w{r u b y}
-      PlayHangman.stub(:get_input => "r")
-      PlayHangman.get_input
-      PlayHangman.playing_the_game(@test)
+  before :each do
+    @test = PlayHangman.new
+    @test.play.secret_word = %w{r u b y}
+    @test.stub(:print_info).and_return("")
+  end
 
-      PlayHangman.stub(:get_input => "u")
-      PlayHangman.get_input
-      PlayHangman.playing_the_game(@test)
+  shared_examples "a Hangman game session" do |input|
+    it "runs through the game" do
+      p input
+      input.each do |letter|
+        PlayHangman.stub(:get_input => letter)
+        PlayHangman.playing_the_game(subject)
 
-      PlayHangman.stub(:get_input => "b")
-      PlayHangman.get_input
-      PlayHangman.playing_the_game(@test)
-
-      PlayHangman.stub(:get_input => "y")
-      PlayHangman.get_input
-      PlayHangman.playing_the_game(@test).should be_true
+      end
     end
+  end
+
+  context "when winning" do
+    subject {@test}
+    it_behaves_like "a Hangman game session", %w{r u b y}
+  end
+
+  context "when losing" do
+    subject {@test}
+    it_behaves_like "a Hangman game session", %w{q a z w s x e d c f v}
+  end
+
+  context "when given invalid input" do
+    subject {@test}
+    it_behaves_like "a Hangman game session", %w{r r ff 6 -}
   end
 end
